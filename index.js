a1f1d6600f3a94ce39c64daba27124d17ef395c1
c2pig <c2pigisme@yahoo.com>
2017-01-24 08:11:22 +0800

    bug fix

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 'use strict';
 module.exports = {
-  validation: require('./src/common/app'),
-  ingest: require('./src/common/app'),
-  moderation: require('./src/common/app'),
-  view: require('./src/common/app'),
   pipe: require('./src/common/app')
 };
--- src/common/db.js
+++ src/common/db.js
@@ -18,5 +18,55 @@ class DB {
     this.db.putItem(params, callback);
   }
 
+  update(key, kvp, table, callback) {
+
+	  const timestamp = new Date().getTime();
+	  let params = {AttributeUpdates: {}};
+	  params.TableName = table;
+	  params.Key = { id : { S: key} };
+
+	  for(var k in kvp) {
+
+		let value = kvp[k];
+		let Value = {} , type = '';
+		if(Array.isArray(value)) {
+			type = "S";
+			let typeCheck = value.pop(0);
+			value.push(typeCheck);
+			console.log("typecheck : " + (typeof typeCheck));
+			if(typeof typeCheck === "number") {
+				type = "N" + type;
+			} else if(typeof typeCheck === "string") {
+				type = "S" + type;
+			}
+
+		} else if(typeof value === "number") {
+			type = "N" + type;
+		} else if(typeof value === "string") {
+			type = "S" + type;
+		} else if(typeof value === "boolean") {
+			type = "BOOL";
+		} else if(typeof value === "object") {
+			type = "M"
+		}
+
+		Value[type] = value;
+
+		params.AttributeUpdates[k] = { Value, Action: "PUT" }
+	  }
+	  params.ReturnValues = "NONE";
+	  console.log(JSON.stringify(params));
+	  this.db.updateItem(params, callback);
+  }
+
+
+  selectByFieldExists(field, table, callback) {
+
+	let params = {
+      "FilterExpression": `attribute_exists(${field})`,
+      "TableName": table
+    };
+    db.scan(params, callback);
+  }
 }
 module.exports = DB;
